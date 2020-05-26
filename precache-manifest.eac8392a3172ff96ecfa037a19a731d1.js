95d7179cc2384b5e0d3bf43c7dadedf6853b4471
Patil <ckpatil@gmail.com>
2020-05-26 15:44:34 +0530

    Updates

---
+++ precache-manifest.eac8392a3172ff96ecfa037a19a731d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8989ad157c1544818ce51bb4d624a80",
+    "url": "/patil-rolodex/index.html"
+  },
+  {
+    "revision": "5640a1d054013f4ebd4d",
+    "url": "/patil-rolodex/static/css/main.89804aa2.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/patil-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/patil-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5640a1d054013f4ebd4d",
+    "url": "/patil-rolodex/static/js/main.8ac3a0da.chunk.js"
+  },
+  {
+    "revision": "6a4c1777205959c6a56a",
+    "url": "/patil-rolodex/static/js/runtime-main.619236ea.js"
+  }
+]);
